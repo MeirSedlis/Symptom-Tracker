5de20d0fc5ae74a283fac213e8898b6c5843961b
Meir Sedlis <meirsedlis@gmail.com>
2022-05-23 16:02:38 -0500

    move symptom detail to item component

---
+++ src/components/SymptomItem/SymptomItem.jsx
@@ -0,0 +1,37 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import IconButton from "@mui/material/IconButton";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import InfoIcon from "@mui/icons-material/Info";   
+
+function SymptomItem({symptom}){
+    return(
+        <Box key={symptom.id}>
+        <Grid item xs={12} md={6}>
+          <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+          </Typography>
+            <List >
+                <ListItem
+                  secondaryAction={
+                    <IconButton edge="end" aria-label="info">
+                      <InfoIcon />
+                    </IconButton>
+                  }
+                >
+                 <Typography>
+                     {symptom.symptom}
+                 </Typography>
+                </ListItem>
+            </List>
+        </Grid>
+      </Box>
+    )
+}
+
+export default SymptomItem; 
--- src/components/SymptomTracker/SymptomTracker.jsx
+++ src/components/SymptomTracker/SymptomTracker.jsx
@@ -2,45 +2,32 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
-import FormGroup from "@mui/material/FormGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import FolderIcon from "@mui/icons-material/Folder";
-import InfoIcon from "@mui/icons-material/Info";
+import InfoIcon from "@mui/icons-material/Info";   
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import SymptomItem from '../SymptomItem/SymptomItem.jsx';
 
 function SymptomTracker() {
+    useEffect(() => {
+        dispatch({
+          type: 'FETCH_SYMPTOMS'
+        })
+      }, [])
+      const symptoms = useSelector(store => store.symptom)
+      const dispatch = useDispatch();
   return (
     <>
-      <Box>
-        <Grid item xs={12} md={6}>
-          <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-          </Typography>
-            <List >
-                <ListItem
-                  secondaryAction={
-                    <IconButton edge="end" aria-label="info">
-                      <InfoIcon />
-                    </IconButton>
-                  }
-                >
-                 <Typography>
-                     something
-                 </Typography>
-                </ListItem>
-            </List>
-        </Grid>
-      </Box>
+    {symptoms.map( (symptom) => {
+        return(
+            <SymptomItem symptom={symptom}/>
+      )}) 
+ }
     </>
   );
 }
