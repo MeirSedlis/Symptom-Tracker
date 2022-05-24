f8738ed627bfa8accce91c58d5d056ad578e3ed4
Meir Sedlis <meirsedlis@gmail.com>
2022-05-24 12:15:44 -0500

    add navigation and styling to track new form

--- src/components/TrackNewForm/TrackNewForm.jsx
+++ src/components/TrackNewForm/TrackNewForm.jsx
@@ -1,8 +1,11 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import { useHistory } from 'react-router';
+import Box from "@mui/material/Box";
 
 function TrackNewForm() {
   useEffect(() => {
@@ -13,17 +16,41 @@ function TrackNewForm() {
 
   const symptoms = useSelector((store) => store.symptom);
   const dispatch = useDispatch();
+  const history = useHistory();
+  const [newSymptom, setNewSymptom] = useState("");
+
+  const handleClick = (e) => {
+    e.preventDefault;
+    console.log(newSymptom);
+  };
 
   return (
-      <>
+    <>
+      <form>
         <Autocomplete
-        id="symptoms"
-        freeSolo
-        options={symptoms.map((option) => option.symptom)}
-        renderInput={(params) => <TextField {...params} label="symptoms" />}
-      />
-      </>
-    )
+          id="symptoms"
+          freeSolo
+          options={symptoms.map((option) => option.symptom)}
+          renderInput={(params) => <TextField {...params} label="symptoms" />}
+        />
+        <Box textAlign="center">
+        <Button size="medium" variant="contained" onClick={handleClick}>
+          Track
+        </Button>
+        <Button
+          size="medium"
+          variant="outlined"
+          color="error"
+          onClick={() => {
+            history.push("/tracker");
+          }}
+        >
+          Cancel
+        </Button>
+        </Box>
+      </form>
+    </>
+  );
 }
 
 export default TrackNewForm;
