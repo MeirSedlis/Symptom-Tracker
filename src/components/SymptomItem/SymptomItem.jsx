e744a5e2fabc70d9fd14857100f83b2249094d5b
Meir Sedlis <meirsedlis@gmail.com>
2022-05-25 14:52:08 -0500

    create intensity dialog, delete old nav

--- src/components/SymptomItem/SymptomItem.jsx
+++ src/components/SymptomItem/SymptomItem.jsx
@@ -8,10 +8,56 @@ import ListItem from "@mui/material/ListItem";
 import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import InfoIcon from "@mui/icons-material/Info";   
+import InfoIcon from "@mui/icons-material/Info"; 
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slider from '@mui/material/Slider'  
+import {useState} from 'react';
 
 function SymptomItem({symptom}){
 
+const logSymptom = () =>{
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
+    const handleSubmit = () =>{
+      setOpen(false);
+    };
+  
+    return (
+            <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Intensity</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Please rate the intensity of this symptom on a scale of 1 (not very intense) to 5 (very intense)
+            </DialogContentText>
+            <Slider
+            aria-label="symptom intensity"
+            defaultValue={1}
+            step={1}
+            marks
+            min={1}
+            max={5}
+            valueLabelDisplay="auto"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleSubmit}>Log</Button>
+          </DialogActions>
+        </Dialog>
+    );
+}
     return(
         <Box key={symptom.id}>
         <Grid item xs={12} md={6}>
@@ -26,7 +72,7 @@ function SymptomItem({symptom}){
                     </IconButton>
                   }
                 >
-                 <Typography>
+                 <Typography onClick={logSymptom}>
                      {symptom.symptom}
                  </Typography>
                 </ListItem>
