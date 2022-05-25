4eb9c996b988cbecf68d1ee7ecf704e27b42b862
Meir Sedlis <meirsedlis@gmail.com>
2022-05-25 16:52:28 -0500

    condense dialog into symptom item, remove redundant dialog component

--- src/components/SymptomItem/SymptomItem.jsx
+++ src/components/SymptomItem/SymptomItem.jsx
@@ -8,80 +8,76 @@ import ListItem from "@mui/material/ListItem";
 import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import InfoIcon from "@mui/icons-material/Info"; 
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Slider from '@mui/material/Slider';  
-import Button from '@mui/material/Button';
-import {useState} from 'react';
+import InfoIcon from "@mui/icons-material/Info";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slider from "@mui/material/Slider";
+import Button from "@mui/material/Button";
+import { useState } from "react";
 
-function SymptomItem({symptom}){
+function SymptomItem({ symptom }) {
+  const [open, setOpen] = useState(false);
 
-const logSymptom = () =>{
-    const [open, setOpen] = useState(true);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClickOpen = () => {
-      setOpen(true);
-    };
-  
-    const handleClose = () => {
-      setOpen(false);
-    };
-  
-    const handleSubmit = () =>{
-      setOpen(false);
-    };
-  
-    return (
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box key={symptom.id}>
+      <Grid item xs={12} md={6}>
+        <Typography
+          sx={{ mt: 4, mb: 2 }}
+          variant="h6"
+          component="div"
+        ></Typography>
+        <List>
+          <ListItem
+            secondaryAction={
+              <IconButton edge="end" aria-label="info">
+                <InfoIcon />
+              </IconButton>
+            }
+          >
+            <Button onClick={handleClickOpen}>{symptom.symptom}</Button>
+
             <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>Intensity</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Please rate the intensity of this symptom on a scale of 1 (not very intense) to 5 (very intense)
-            </DialogContentText>
-            <Slider
-            aria-label="symptom intensity"
-            defaultValue={1}
-            step={1}
-            marks
-            min={1}
-            max={5}
-            valueLabelDisplay="auto"
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button onClick={handleSubmit}>Log</Button>
-          </DialogActions>
-        </Dialog>
-    );
-}
-    return(
-        <Box key={symptom.id}>
-        <Grid item xs={12} md={6}>
-          <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-          </Typography>
-            <List >
-                <ListItem
-                
-                  secondaryAction={
-                    <IconButton edge="end" aria-label="info">
-                      <InfoIcon />
-                    </IconButton>
-                  }
-                >
-                 <Button onClick={logSymptom}>
-                     {symptom.symptom}
-                 </Button>
-                 
-                </ListItem>
-            </List>
-        </Grid>
-      </Box>
-    )
+              <DialogTitle>Intensity</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Please rate the intensity of this symptom on a scale of 1 (not
+                  very intense) to 5 (very intense)
+                </DialogContentText>
+                <Slider
+                  aria-label="symptom intensity"
+                  defaultValue={1}
+                  step={1}
+                  marks
+                  min={1}
+                  max={5}
+                  valueLabelDisplay="auto"
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={handleSubmit}>Log</Button>
+              </DialogActions>
+            </Dialog>
+          </ListItem>
+        </List>
+      </Grid>
+    </Box>
+  );
 }
 
-export default SymptomItem; 
+export default SymptomItem;
